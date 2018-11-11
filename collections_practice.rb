188fdd5436610b90e8fbdf3d099e1865f4ed80b4
Dmitri Samotessov <dimasamo96@gmail.com>
2018-11-11 06:09:23 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -62,4 +62,10 @@ def find_cool(array)
 end
 
 def organize_schools(schools)
+  organized_schools={}
+  schools.each do |name, data|
+    if !organized_schools.has_key?(data[:location])
+
+    end
+  end
 end
